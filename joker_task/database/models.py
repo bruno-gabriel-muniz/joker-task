e47f5fc12a96de9c803b0e91a55de4a317675224
bruno-gabriel-muniz <brunomunizlourenco@gmail.com>
2025-10-13 19:37:48 -0300

    feat: atualiza as colunas e adiciona create e updated a tabela tasks

--- joker_task/database/models.py
+++ joker_task/database/models.py
@@ -37,11 +37,24 @@ class Task:
     id_task: Mapped[int] = mapped_column(
         Integer, primary_key=True, autoincrement=True
     )
-    email_user: Mapped[str] = mapped_column(ForeignKey('users.email'))
+    user_email: Mapped[str] = mapped_column(ForeignKey('users.email'))
     user: Mapped['User'] = relationship(back_populates='tasks')
-    done: Mapped[bool] = mapped_column(Boolean)
-    tag: Mapped[str] = mapped_column(String)
-    reminder: Mapped[datetime] = mapped_column(DateTime)
-    repetition: Mapped[str] = mapped_column(String)  # Implementação Temporária
-    state: Mapped[str] = mapped_column(String)  # Kanban
+
+    done: Mapped[bool] = mapped_column(Boolean, nullable=True)
+    tag: Mapped[str] = mapped_column(String, nullable=True)
+    reminder: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    repetition: Mapped[str] = mapped_column(
+        String, nullable=True
+    )  # Implementação Temporária
+    state: Mapped[str] = mapped_column(String, nullable=True)  # Kanban
     priority: Mapped[int] = mapped_column(Integer, default=100)
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        init=False,
+        server_default=func.now(),
+        server_onupdate=func.now(),
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, init=False, server_default=func.now()
+    )
